8fb8e04b5d77bb085ee8a31c372c5f2437e8aad9
Thomas Shoff <thomasshoff14@gmail.com>
2019-03-01 12:52:03 -0500

    Logic for existing files, implemented initial data visuals

--- mainscript.py
+++ mainscript.py
@@ -2,6 +2,9 @@ import MboxParser as Mbox
 import mailbox
 import time
 import os
+import pandas as pd
+import numpy as np
+
 
 # Keywords to flag for
 KEY_WORDS = ('Amazon', 'Walmart', 'Target', 'BestBuy', 'Ebay', 'Etsy', 'Alixpress', 'Costco', 'Kohls', 'Sears',
@@ -15,32 +18,39 @@ ILLEGAL_CHARACTERS = ('/', ':', '*', '?', '<', '>', '|', '\\', ' ', '"', '-', '\
 mbox_filepath = 'C:\\Mail\\BigFile.mbox'
 
 # Set flagged output path here
-flagged_path = 'FlaggedEmails.txt'
+flagged_path = 'C:\\Users\\Thomas\\Documents\\Programming\\WHI Lab\\Outputs\\FlaggedEmails.txt'
 
 # Set normal output path here
-normal_path = 'NormalEmails.txt'
+normal_path = 'C:\\Users\\Thomas\\Documents\\Programming\\WHI Lab\\Outputs\\NormalEmails.txt'
+
 
 
 if __name__ == '__main__':
 
-    if os.path.getsize(flagged_path) != 0 or os.path.getsize(normal_path) != 0:
-        answer = input('Output files have data. Overwrite? (y/n): ')
-        if answer == 'y':
-            answer = input('Are you sure? (y/n): ')
+    # Checks if output files exist. If existing, gives option to overwrite or append
+    try:
+        if os.path.getsize(flagged_path) != 0 or os.path.getsize(normal_path) != 0:
+            answer = input('Output files have data. Overwrite? (y/n): ')
             if answer == 'y':
-                file = open(flagged_path, 'w', encoding='utf-8')
-                file.close()
-                file = open(normal_path, 'w', encoding='utf-8')
-                file.close()
-                print('Files wiped')
+                answer = input('Are you sure? (y/n): ')
+                if answer == 'y':
+                    file = open(flagged_path, 'w', encoding='utf-8')
+                    file.close()
+                    file = open(normal_path, 'w', encoding='utf-8')
+                    file.close()
+                    print('Files wiped')
+                else:
+                    print('Files kept, new data will be appended')
             else:
                 print('Files kept, new data will be appended')
-        else:
-            print('Files kept, new data will be appended')
+    except IOError:
+        print('Output files not found, ignoring...')
+
 
     start_time = time.time()
     print('Running...')
 
+    data_list = []
     emails = mailbox.mbox(mbox_filepath)
     for email in emails:
         flagged = []
@@ -57,8 +67,11 @@ if __name__ == '__main__':
             file = open(normal_path, 'a', encoding='utf-8')
             file.write(parsed.to_string())
             file.close()
-        print(parsed.to_string())
+        data_list.append(parsed.to_dict())
 
     elapsed_time = time.time() - start_time
-    print('Complete')
+    print('Parsing Complete')
     print(elapsed_time)
+
+    df = pd.DataFrame(data_list)
+    print(df)
